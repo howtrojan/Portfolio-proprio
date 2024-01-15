b63629552c1896df3779c9191f597485c4f47103
Howtrojan <andre.braga.asb@gmail.com>
2024-01-15 17:07:30 -0300

    Incluido efeito de escrever e apagar

--- js/bemvindo.js
+++ js/bemvindo.js
@@ -1,13 +1,29 @@
 var div = document.getElementById('bemvindo');
 var texto = 'Em busca da primeira oportunidade como programador Front-End';
+let direcao = 1; // 1 para escrever, -1 para apagar
+let cursor = '|';
 
-function escrever(str, el) {
-  var char = str.split('').reverse();
-  var typer = setInterval(function() {
-    if (!char.length) return clearInterval(typer);
-    var next = char.pop();
-    el.innerHTML += next;
-  }, 50);
+function escreverApagar(str, el) {
+  let char = str.split('');
+  let index = 0;
+
+  setInterval(function () {
+    if (direcao === 1) {
+      if (index <= str.length) {
+        el.innerHTML = str.substring(0, index) + cursor;
+        index++;
+      } else {
+        direcao = -1;
+      }
+    } else {
+      if (index >= 0) {
+        el.innerHTML = str.substring(0, index) + cursor;
+        index--;
+      } else {
+        direcao = 1;
+      }
+    }
+  }, 80);
 }
 
-escrever(texto, div);
+escreverApagar(texto, div);
